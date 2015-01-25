0de893a00ea14b6229ffd7627833bba44485866e
Suresh <suresh.kumar@rebtel.com>
2015-01-25 21:35:02 +0100

    commit

--- Profiling/app/src/main/java/user/com/profiling/MainActivity.java
+++ Profiling/app/src/main/java/user/com/profiling/MainActivity.java
@@ -5,12 +5,17 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.preference.PreferenceManager;
 import android.provider.Browser;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import com.android.volley.Response;
@@ -30,95 +35,131 @@ import user.com.profiling.network.response.UserProfiling;
 
 public class MainActivity extends ActionBarActivity {
 
-	private String TAG = MainActivity.class.getSimpleName();
-	private String emailOffers = "{\n" +
-			"    \"Emails\": [\n" +
-			"        {\n" +
-			"            \"US\": [\n" +
-			"                {\n" +
-			"                    \"profile\": \"Dater\",\n" +
-			"                    \"Subject\": \"New Dater message for you\",\n" +
-			"                    \"Body\": \"Check this Link-> HYPERLINK\",\n" +
-			"                    \"Link\": \"http://www.date.com\",\n" +
-			"                    \"Unsubscribe\": \"If you wish to stop getting this   mails -> HYPERLINK\"\n" +
-			"                },\n" +
-			"                {\n" +
-			"                    \"profile\": \"Gamer\",\n" +
-			"                    \"Subject\": \"New Gamer message for you\",\n" +
-			"                    \"Body\": \"Check this Link-> HYPERLINK\",\n" +
-			"                    \"Link\": \"http://www.poker.com\",\n" +
-			"                    \"Unsubscribe\": \"If you wish to stop getting this   mails -> HYPERLINK\"\n" +
-			"                },\n" +
-			"                {\n" +
-			"                    \"profile\": \"Dater\",\n" +
-			"                    \"Subject\": \"New Dater2 message for you\",\n" +
-			"                    \"Body\": \"Check this Link-> HYPERLINK\",\n" +
-			"                    \"Link\": \"http://www.cupid.com\",\n" +
-			"                    \"Unsubscribe\": \"If you wish to stop getting this   mails -> HYPERLINK\"\n" +
-			"                }\n" +
-			"            ]\n" +
-			"        },\n" +
-			"            {\n" +
-			"                \"UK\": [\n" +
-			"                    {\n" +
-			"                        \"profile\": \"Dater\",\n" +
-			"                        \"Subject\": \"New Dater message for you\",\n" +
-			"                        \"Body\": \"Check this Link-> HYPERLINK\",\n" +
-			"                        \"Link\": \"http://www.date.com\",\n" +
-			"                        \"Unsubscribe\": \"If you wish to stop getting this   mails -> HYPERLINK\"\n" +
-			"                    },\n" +
-			"                    {\n" +
-			"                        \"profile\": \"Gamer\",\n" +
-			"                        \"Subject\": \"New Gamer message for you\",\n" +
-			"                        \"Body\": \"Check this Link-> HYPERLINK\",\n" +
-			"                        \"Link\": \"http://www.poker.com\",\n" +
-			"                        \"Unsubscribe\": \"If you wish to stop getting this   mails -> HYPERLINK\"\n" +
-			"                    },\n" +
-			"                    {\n" +
-			"                        \"profile\": \"Dater\",\n" +
-			"                        \"Subject\": \"New Dater2 message for you\",\n" +
-			"                        \"Body\": \"Check this Link-> HYPERLINK\",\n" +
-			"                        \"Link\": \"http://www.cupid.com\",\n" +
-			"                        \"Unsubscribe\": \"If you wish to stop getting this   mails -> HYPERLINK\"\n" +
-			"                    }\n" +
-			"                ]\n" +
-			"            },\n" +
-			"                {\n" +
-			"                    \"Default\": [\n" +
-			"                        {\n" +
-			"                            \"profile\": \"Default\",\n" +
-			"                            \"Subject\": \"New Deals for you\",\n" +
-			"                            \"Body\": \"Check this Link-> HYPERLINK\",\n" +
-			"                            \"Link\": \"http://www.Dealextreme.com\",\n" +
-			"                            \"Unsubscribe\": \"If you wish to stop getting this   mails -> HYPERLINK\"\n" +
-			"                        }\n" +
-			"                    ]\n" +
-			"                }],\n" +
-			"                \"NextIntervalInHours\": \"5\"\n" +
-			"            }";
+    private static final String CONFIRM_DIALOG_SHOWN = "confirmDialogShown";
+    private String TAG = MainActivity.class.getSimpleName();
+    private String emailOffers = "{\n" +
+            "    \"Emails\": [\n" +
+            "        {\n" +
+            "            \"US\": [\n" +
+            "                {\n" +
+            "                    \"profile\": \"Dater\",\n" +
+            "                    \"Subject\": \"New Dater message for you\",\n" +
+            "                    \"Body\": \"Check this Link-> HYPERLINK\",\n" +
+            "                    \"Link\": \"http://www.date.com\",\n" +
+            "                    \"Unsubscribe\": \"If you wish to stop getting this   mails -> HYPERLINK\"\n" +
+            "                },\n" +
+            "                {\n" +
+            "                    \"profile\": \"Gamer\",\n" +
+            "                    \"Subject\": \"New Gamer message for you\",\n" +
+            "                    \"Body\": \"Check this Link-> HYPERLINK\",\n" +
+            "                    \"Link\": \"http://www.poker.com\",\n" +
+            "                    \"Unsubscribe\": \"If you wish to stop getting this   mails -> HYPERLINK\"\n" +
+            "                },\n" +
+            "                {\n" +
+            "                    \"profile\": \"Dater\",\n" +
+            "                    \"Subject\": \"New Dater2 message for you\",\n" +
+            "                    \"Body\": \"Check this Link-> HYPERLINK\",\n" +
+            "                    \"Link\": \"http://www.cupid.com\",\n" +
+            "                    \"Unsubscribe\": \"If you wish to stop getting this   mails -> HYPERLINK\"\n" +
+            "                }\n" +
+            "            ]\n" +
+            "        },\n" +
+            "            {\n" +
+            "                \"UK\": [\n" +
+            "                    {\n" +
+            "                        \"profile\": \"Dater\",\n" +
+            "                        \"Subject\": \"New Dater message for you\",\n" +
+            "                        \"Body\": \"Check this Link-> HYPERLINK\",\n" +
+            "                        \"Link\": \"http://www.date.com\",\n" +
+            "                        \"Unsubscribe\": \"If you wish to stop getting this   mails -> HYPERLINK\"\n" +
+            "                    },\n" +
+            "                    {\n" +
+            "                        \"profile\": \"Gamer\",\n" +
+            "                        \"Subject\": \"New Gamer message for you\",\n" +
+            "                        \"Body\": \"Check this Link-> HYPERLINK\",\n" +
+            "                        \"Link\": \"http://www.poker.com\",\n" +
+            "                        \"Unsubscribe\": \"If you wish to stop getting this   mails -> HYPERLINK\"\n" +
+            "                    },\n" +
+            "                    {\n" +
+            "                        \"profile\": \"Dater\",\n" +
+            "                        \"Subject\": \"New Dater2 message for you\",\n" +
+            "                        \"Body\": \"Check this Link-> HYPERLINK\",\n" +
+            "                        \"Link\": \"http://www.cupid.com\",\n" +
+            "                        \"Unsubscribe\": \"If you wish to stop getting this   mails -> HYPERLINK\"\n" +
+            "                    }\n" +
+            "                ]\n" +
+            "            },\n" +
+            "                {\n" +
+            "                    \"Default\": [\n" +
+            "                        {\n" +
+            "                            \"profile\": \"Default\",\n" +
+            "                            \"Subject\": \"New Deals for you\",\n" +
+            "                            \"Body\": \"Check this Link-> HYPERLINK\",\n" +
+            "                            \"Link\": \"http://www.Dealextreme.com\",\n" +
+            "                            \"Unsubscribe\": \"If you wish to stop getting this   mails -> HYPERLINK\"\n" +
+            "                        }\n" +
+            "                    ]\n" +
+            "                }],\n" +
+            "                \"NextIntervalInHours\": \"5\"\n" +
+            "            }";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		Intent service = new Intent(getApplicationContext(), ProfilingSchedulingService.class);
-		startService(service);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.confirm_dialog);
+        Offer offer = new Gson().fromJson(emailOffers, EmailOffers.class).getEmails().get(0).get("US").get(0);
+        initView();
+    }
 
-		Offer offer = new Gson().fromJson(emailOffers, EmailOffers.class).getEmails().get(0).get("US").get(0);
-		((TextView) findViewById(R.id.content)).setText(offer.getBody());
-	}
+    private void initView() {
+        final Button confirmBtn = (Button) findViewById(R.id.confirm);
+        confirmBtn.setEnabled(false);
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit().putBoolean(CONFIRM_DIALOG_SHOWN, true).commit();
+                Intent service = new Intent(getApplicationContext(), ProfilingSchedulingService.class);
+                startService(service);
+                finish();
+            }
+        });
 
-	private void getEmailOffers() {
-		ApiRequests.getEmailOffers(getApplicationContext(), new Response.Listener<EmailOffers>() {
-			@Override
-			public void onResponse(EmailOffers emailOffers) {
-				Log.d(TAG, emailOffers.getEmails().get(0).get("US").toString());
-			}
-		}, new Response.ErrorListener() {
-			@Override
-			public void onErrorResponse(VolleyError volleyError) {
-				Log.d(TAG, String.valueOf(volleyError.getMessage()));
-			}
-		});
-	}
+        Button bLater = (Button) findViewById(R.id.confirmLater);
+        bLater.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        final CheckBox checkBox = (CheckBox) findViewById(R.id.confirmCheckBox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    confirmBtn.setEnabled(true);
+                } else {
+                    confirmBtn.setEnabled(false);
+                }
+            }
+        });
+        findViewById(R.id.confirmText).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkBox.toggle();
+            }
+        });
+    }
+
+    private void getEmailOffers() {
+        ApiRequests.getEmailOffers(getApplicationContext(), new Response.Listener<EmailOffers>() {
+            @Override
+            public void onResponse(EmailOffers emailOffers) {
+                Log.d(TAG, emailOffers.getEmails().get(0).get("US").toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Log.d(TAG, String.valueOf(volleyError.getMessage()));
+            }
+        });
+    }
 }
